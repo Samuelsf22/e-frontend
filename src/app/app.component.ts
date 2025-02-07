7c929c74f4354ae40c46503815fb299cbe72b8cd
SamuelSF22 <138950240+Samuelsf22@users.noreply.github.com>
2025-02-06 19:44:45 -0500

    feat: add theme toggle component and update UI button module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { ThemeToggleComponent } from './theme-toggle/theme-toggle.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, HlmButtonDirective, ThemeToggleComponent],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'e-frontend';
