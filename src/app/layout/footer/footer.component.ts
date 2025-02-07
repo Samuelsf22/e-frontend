0c652cfba6eaea3094f4de4717ca4368f78910a8
SamuelSF22 <138950240+Samuelsf22@users.noreply.github.com>
2025-02-06 23:01:03 -0500

    feat: implement home component and update routing; enhance footer layout and styles

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { featherGithub, featherTruck } from '@ng-icons/feather-icons';
+import { featherGithub } from '@ng-icons/feather-icons';
 
 @Component({
   selector: 'e-footer',
   imports: [NgIcon],
   templateUrl: './footer.component.html',
-  viewProviders: [provideIcons({ featherGithub, featherTruck })],
+  viewProviders: [provideIcons({ featherGithub })],
 })
 export class FooterComponent {
   currentYear = new Date().getFullYear();
