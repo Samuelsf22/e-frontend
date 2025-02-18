a0e232efc5b014f22e4bba2283184abc1bc4713c
SamuelSF22 <138950240+Samuelsf22@users.noreply.github.com>
2025-02-17 22:08:07 -0500

    feat: enhance product card component with routing and detailed display

--- src/app/product/card/card.component.ts
+++ src/app/product/card/card.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Product } from '@shared/model/product.model';
+import {
+  HlmCardContentDirective,
+  HlmCardDescriptionDirective,
+  HlmCardDirective,
+  HlmCardHeaderDirective,
+  HlmCardTitleDirective,
+} from '@spartan-ng/ui-card-helm';
+import { HlmMutedDirective } from '@spartan-ng/ui-typography-helm';
 
 @Component({
   selector: 'product-card',
-  imports: [],
+  imports: [
+    RouterLink,
+    HlmCardDirective,
+    HlmCardContentDirective,
+    HlmCardHeaderDirective,
+    HlmCardTitleDirective,
+    HlmCardDescriptionDirective,
+    CommonModule,
+    HlmMutedDirective,
+  ],
   templateUrl: './card.component.html',
 })
 export class ProductCardComponent {
-
+  product = input.required<Product>();
 }
