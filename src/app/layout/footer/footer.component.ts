1649c7572e87c2fc0b25b5b22a5cf443754f016c
SamuelSF22 <138950240+Samuelsf22@users.noreply.github.com>
2025-03-02 12:40:31 -0500

    feat: enhance footer with button for GitHub link

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { featherGithub } from '@ng-icons/feather-icons';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 
 @Component({
   selector: 'e-footer',
-  imports: [NgIcon],
+  imports: [NgIcon, HlmButtonDirective],
   templateUrl: './footer.component.html',
   viewProviders: [provideIcons({ featherGithub })],
 })
