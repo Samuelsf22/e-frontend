21ab7301f633d5e507d204055894d2a765091200
SamuelSF22 <138950240+Samuelsf22@users.noreply.github.com>
2025-03-05 22:27:29 -0500

    feat: enhance cart component with item summary and total calculation

--- src/app/order/cart/cart.component.ts
+++ src/app/order/cart/cart.component.ts
@@ -2,17 +2,20 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   HlmCardDirective,
-  HlmCardContentDirective,
-  HlmCardTitleDirective,
   HlmCardHeaderDirective,
+  HlmCardTitleDirective,
+  HlmCardContentDirective,
+  HlmCardDescriptionDirective,
 } from '@spartan-ng/ui-card-helm';
+import { HlmSeparatorDirective } from '@spartan-ng/ui-separator-helm';
+import { BrnSeparatorComponent } from '@spartan-ng/brain/separator';
 import { injectQuery } from '@tanstack/angular-query-experimental';
 import { ProductService } from '@shared/service/api/product.service';
 import { HlmSpinnerModule } from '@spartan-ng/ui-spinner-helm';
 import { HlmPDirective } from '@spartan-ng/ui-typography-helm';
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { featherMinus, featherPlus } from '@ng-icons/feather-icons';
+import { featherMinus, featherPlus, featherTrash2 } from '@ng-icons/feather-icons';
 import { CartItem } from '@shared/model/product.model';
 import { lastValueFrom } from 'rxjs';
 
@@ -21,14 +24,19 @@ import { lastValueFrom } from 'rxjs';
   imports: [
     CommonModule,
     HlmCardDirective,
+    HlmCardHeaderDirective,
     HlmCardTitleDirective,
+    HlmCardDescriptionDirective,
+    HlmCardContentDirective,
     HlmSpinnerModule,
     HlmPDirective,
     HlmButtonDirective,
     NgIcon,
+    HlmSeparatorDirective,
+    BrnSeparatorComponent,
   ],
   templateUrl: './cart.component.html',
-  providers: provideIcons({ featherPlus, featherMinus }),
+  providers: provideIcons({ featherPlus, featherMinus, featherTrash2 }),
 })
 export class CartComponent {
   private productService = inject(ProductService);
@@ -54,4 +62,12 @@ export class CartComponent {
     this.productService.removeFromCart(public_id);
     this.cartQuery.refetch();
   }
+
+  computeTotal() {
+    return this.cartQuery.data()?.reduce(
+      (acc: number, item: CartItem) => acc + item.price * item.quantity!,
+      0
+    );
+  }
+
 }
