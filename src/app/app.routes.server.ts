5ee16f178ddbb11cda39616e862ffbed8084ee81
SamuelSF22 <138950240+Samuelsf22@users.noreply.github.com>
2025-03-07 00:10:47 -0500

    feat: add server route for orders page with prerendering support

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,14 +3,18 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Prerender,
   },
   {
     path: 'product/:public_id',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Server,
   },
   {
     path: 'category/:public_id',
-    renderMode: RenderMode.Server
-  }
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'orders/:public_id',
+    renderMode: RenderMode.Server,
+  },
 ];
